b0eac5001d62574664b2d7cdf2f6aa703dfa768e
Mario Portilho <junior.portilho2005@gmail.com>
2025-04-03 19:02:46 +0100

    index page

---
+++ components/footer/Paragraph.tsx
@@ -0,0 +1,14 @@
+export default function Paragraph() {
+    return (
+        <>
+            <div className="flex flex-col">
+                <h1 className="font-bold">LalalA</h1>
+                <div className="text-sm gap-1 flex flex-col">
+                    <p>Lalala</p>
+                    <p>Lalala</p>
+                    <p>Lalala</p>
+                </div>
+            </div>
+        </>
+    );
+}
